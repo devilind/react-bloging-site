15a909c45dfa9372e1b03efb56ffbba8e7f22f3f
devilind <atul.eathour.98@gmail.com>
2023-12-03 14:37:11 +0530

    1.0.5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import React from 'react'
 import Header from './common/Header.js';
 import Footer from './common/Footer.js';
@@ -7,38 +7,32 @@ import Home from './pages/Home/Home.js';
 import ContactUs from './pages/ContactUs/ContactUs.js';
 import About from './pages/About/About.js';
 import NotFound from "./NotFound/NotFound.js";
-import '../src/App.css';
+import './App.css';
 import BlogHomePage from "./pages/Bloghome/BlogHomePage.js";
-// import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 function App() {
-  
+
   return (
-    
-    <div className='linear-gradient-bg'>
-      <div className='p-4'>
-        <Header />
+    <body>
+      <div className='linear-gradient-bg'>
+        <div className='p-4'>
+          <Header />
+        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blog' element={<BlogHomePage />} />
+          <Route path='/market' element={<BlogBody />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<ContactUs />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+        <div>
+          <Footer />
+        </div>
       </div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/blog' element={<BlogHomePage />} />
-        <Route path='/market'  element={<BlogBody />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<ContactUs />} />
-        <Route path='/*' element={<NotFound />} />
-        
-      </Routes>
-      <div>
-        <Footer />
-      </div>
-    </div>
-
-    
-
-
-
+    </body>
   );
 }
 
